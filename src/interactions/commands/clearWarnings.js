77c6a23e4640d1259f76cb65f7a1601479ac09a2
xretribution237-lgtm <xretribution237@gmail.com>
2026-03-03 21:42:40 -0500

    Delete src directory

--- src/interactions/commands/clearWarnings.js
+++
@@ -1,7 +0,0 @@
-const { state } = require("../../config");
-
-module.exports = async function clearWarnings(interaction) {
-  const member = interaction.options.getMember("member");
-  delete state.warnLog[member.id];
-  await interaction.reply({ content: `✅ All warnings cleared for **${member.displayName}**.`, flags: 64 });
-};
--- src/interactions/commands/giveRole.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function giveRole(interaction) {
-  const member = interaction.options.getMember("member");
-  const role   = interaction.options.getRole("role");
-  await member.roles.add(role);
-  await interaction.reply({ content: `✅ Gave **${role.name}** to **${member.displayName}**.`, flags: 64 });
-};
